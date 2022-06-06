72994f1da7fa726794d91bc29292eda5317aa53c
99Hassan20 <hassanoigag0@gmail.com>
2022-06-06 22:28:09 +0100

    done with the markdwon

--- markdown.py
+++ markdown.py
@@ -1,4 +1,4 @@
-def markdown_header():
+def get_markdown_header():
     while True:
         try:
             level = int(input("Level: "))
@@ -13,11 +13,33 @@ def markdown_header():
     markdown = f"{'#' * level} {text}\n"
     return markdown
 
-def mardown_link():
+def get_markdown_list(list_type):
+    list_items = []
+    while True:
+        try:
+            rows = int(input("Number of rows: "))
+        except ValueError:
+            print("digits please")
+            continue
+        if rows <= 0:
+            print("The number of rows should be greater than zero")
+        else:
+            break
+    for i in range(rows):
+        row = input(f"Row #{i + 1}: ")
+        list_items.append(row)
+    if list_type == "ordered-list":
+        markdown = list(map(lambda item: f"{item[0] + 1}. {item[1]}\n", enumerate(list_items)))
+    else:
+        markdown = list(map(lambda item: f"* {item}\n", list_items))
+    return ''.join(markdown)
+
+def get_markdown_link():
         label = input("Label: ")
         url = input("URL: ")
         return f"[{label}]({url})"
-def mardown_text(markdown_type):
+        
+def get_markdown_text(markdown_type):
     text = input("Text: ")
     markdown = ""
     if markdown_type == "bold":
@@ -30,8 +52,25 @@ def mardown_text(markdown_type):
         markdown = text
     return markdown
 
+def save_to_file(markdown):
+    with open("output.md", "w") as md:
+        md.write(markdown)
+
+def get_markdown(formatter):
+    if formatter == "header":
+        markdown = get_markdown_header()
+    elif formatter in ["plain", "bold", "italic", "inline-code"]:
+        markdown = get_markdown_text(formatter)
+    elif formatter == "new-line":
+        markdown = "\n"
+    elif formatter == "link":
+        markdown = get_markdown_link()
+    elif formatter in ["ordered-list", "unordered-list"]:
+        markdown = get_markdown_list(formatter)
+    return markdown
+
 if __name__ == "__main__":
-    available_formatters = ["plain", "bold", "italic", "header", "link", "inline-code", "new-line"]
+    available_formatters = ["plain", "bold", "italic", "header", "link", "inline-code", "new-line", "ordered-list", "unordered-list"]
     special_commands = ["!help", "!done"]
     markdown = ""
     while True:
@@ -39,20 +78,11 @@ if __name__ == "__main__":
         if formatter not in available_formatters + special_commands:
             print("Unknown formatting type or command")
         elif formatter == "!done":
+            save_to_file(markdown)
             break
         elif formatter == "!help":
             print("Available formatters:", " ".join(available_formatters))
             print("Special commands:", " ".join(special_commands))
         else:
-            if formatter == "header":
-                markdown += markdown_header()
-                print(markdown)
-            elif formatter in ["plain", "bold", "italic", "inline-code"]:
-                markdown += mardown_text(formatter)
-                print(markdown)
-            elif formatter == "new-line":
-                markdown += "\n"
-                print(markdown)
-            elif formatter == "link":
-                markdown += mardown_link()
-                print(markdown)
+            markdown += get_markdown(formatter)
+            print(markdown)
